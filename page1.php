e8e3bc527409a198662410a40281e0bbdcd5aff6
Piamsuk <97584435+Piamsuk@users.noreply.github.com>
2022-02-01 16:54:34 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -3,7 +3,7 @@
       <title>page1</title>
    </head>
    <body bgcolor="lightblue">
-     <h1 style="colore:darkblue;">ประวัติส่วนตัว</h1>
+     <h1 style="color:darkblue;">ประวัติส่วนตัว</h1>
       <p><b>ชื่อ:</b> <i>นางสาวเปี่ยมสุข ศึกษา</i></p>
       <p><b>ชื่อเล่น:</b> <i>พาย</i></p>
       <p><b>ชั้น:</b> <i>ม.4/12</i></p>
