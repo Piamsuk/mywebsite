64376376a956dda274a32bc4f9e222732558e188
Piamsuk <97584435+Piamsuk@users.noreply.github.com>
2022-02-09 14:40:48 +0700

    Update link.php

--- link.php
+++ link.php
@@ -5,7 +5,7 @@
   <body>
     <a href="https://lomsak.ac.th"><h1>วาร์ปของฉัน</h1></a>
     <a href="contact.php"><h1>ติดต่อฉัน</h1></a>
-    <a href="#footer"<h1>ไปด้านล่าง</h1></a>
+    <a href="#footer"<h1>ไปด้านล่าง</h1></a><br>
     Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>
     Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>
     Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>Piamsuk<br>
