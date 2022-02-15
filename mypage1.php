e4c201b44dd0bec1f4d2c8ab370b1781c82f3f56
Piamsuk <97584435+Piamsuk@users.noreply.github.com>
2022-02-15 14:49:48 +0700

    Update mypage1.php

--- mypage1.php
+++ mypage1.php
@@ -4,6 +4,6 @@
   </head>
   <body>
     <a href="https://facebook.com"><h1>Facebook</h1></a>
-    <a href="mypage2.php"><h1>Page2</h1></a>
+    <a href="mypage2.php"><h1>Page 2</h1></a>
   </body>
 </html>
