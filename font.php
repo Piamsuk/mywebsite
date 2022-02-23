89affdd54f668c014b7d22c2e9ec13120d691f40
Piamsuk <97584435+Piamsuk@users.noreply.github.com>
2022-02-23 13:15:51 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("
+  </head>
+  <body>
+    
+  </body>
+</html>
