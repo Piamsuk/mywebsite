861e4423d1dd98884a07fb583c8f22302185bc52
Piamsuk <97584435+Piamsuk@users.noreply.github.com>
2022-02-23 13:22:54 +0700

    Update font.php

--- font.php
+++ font.php
@@ -4,9 +4,14 @@
     <style>
       @font-face{
         font-family: "MyFont1";
-        src: url("
+        src: url("FC Minimal Italic.ttf");
+      }
+      #font1{
+        font-family: "MyFont1";
+      }
+    </style>
   </head>
   <body>
-    
+    <h1>ฟอนต์ของฉัน</h1>
   </body>
 </html>
