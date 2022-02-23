744928059719f76f382d8ac05b51c1d5673e5a68
Piamsuk <97584435+Piamsuk@users.noreply.github.com>
2022-02-23 13:42:09 +0700

    Update font.php

--- font.php
+++ font.php
@@ -6,12 +6,20 @@
         font-family: "MyFont1";
         src: url("FC Minimal Italic.ttf");
       }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("baozi.ttf");
+      }
       #font1{
         font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
       }
     </style>
   </head>
   <body>
     <h1 id="font1">ฟอนต์ของฉัน</h1>
+    <h1 id="font2">เปี่ยมสุข</h1>
   </body>
 </html>
