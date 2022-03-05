6a2e3b19846e48d7990e7b6631b3eddbdaea9f22
Piamsuk <97584435+Piamsuk@users.noreply.github.com>
2022-03-05 14:56:48 +0700

    Update table.php

--- table.php
+++ table.php
@@ -13,31 +13,31 @@
       <tr>
         <td>1</td>
         <td>Mark Zuckerberg</td>
-        <td><img src="https://tse2.mm.bing.net/th?id=OIP.yV3JKvyb4qERiIfaGPvUBAHaE7&pid=Api&P=0&w=258&h=172" width="45px"></td>
+        <td><img src="https://tse2.mm.bing.net/th?id=OIP.yV3JKvyb4qERiIfaGPvUBAHaE7&pid=Api&P=0&w=258&h=172" width="65px"></td>
         <td><a href="https://www.facebook.com/search/top?q=mark%20zuckerberg">FB</td>
       </tr>
       <tr>
         <td>1</td>
-        <td>Lalisa Manoban</td>
-        <td><img src="https://pbs.twimg.com/profile_images/1387065127208247299/bni08CVZ_400x400.jpg" width="45px"></td>
+        <td>Billie Eilish</td>
+        <td><img src="https://pbs.twimg.com/profile_images/1387065127208247299/bni08CVZ_400x400.jpg" width="65px"></td>
         <td><a href="https://www.facebook.com/billieeilish">FB</td>
       </tr>
       <tr>
         <td>1</td>
         <td>Melanie Martinez</td>
-        <td><img src="https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/c/0/6/e/c06e5b6d6a73bd671dd40e3f4e5b43fb.jpg" width="45px"></td>
+        <td><img src="https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/c/0/6/e/c06e5b6d6a73bd671dd40e3f4e5b43fb.jpg" width="65px"></td>
         <td><a href="https://www.facebook.com/melaniemartinezmusic">FB</td>
       </tr>
       <tr>
         <td>1</td>
         <td>Lana Del Rey</td>
-        <td><img src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Lana_Del_Rey_Releases_Music_Video_For_New_Track_%27Burning_Desire%279.jpg"></td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Lana_Del_Rey_Releases_Music_Video_For_New_Track_%27Burning_Desire%279.jpg" width="65px"></td>
         <td><a href="https://www.facebook.com/melaniemartinezmusic">FB</td>
       </tr>
       <tr>
         <td>1</td>
         <td>AURORA</td>
-        <td><img src="https://upload.wikimedia.org/wikipedia/commons/f/fe/Aurora_Stavernfestivalen_2018_%28191706%29_%28cropped%29.jpg" width="45px"></td>
+        <td><img src="https://upload.wikimedia.org/wikipedia/commons/f/fe/Aurora_Stavernfestivalen_2018_%28191706%29_%28cropped%29.jpg" width="65px"></td>
         <td><a href="https://www.facebook.com/iamAURORA/">FB</td>
       </tr>
     </table>
