6fc24a517228241aca959063bfa877fdf4bb9bdf
Piamsuk <97584435+Piamsuk@users.noreply.github.com>
2022-03-05 15:01:35 +0700

    Update table.php

--- table.php
+++ table.php
@@ -17,25 +17,25 @@
         <td><a href="https://www.facebook.com/search/top?q=mark%20zuckerberg">FB</td>
       </tr>
       <tr>
-        <td>1</td>
+        <td>2</td>
         <td>Billie Eilish</td>
         <td><img src="https://pbs.twimg.com/profile_images/1387065127208247299/bni08CVZ_400x400.jpg" width="65px"></td>
         <td><a href="https://www.facebook.com/billieeilish">FB</td>
       </tr>
       <tr>
-        <td>1</td>
+        <td>3</td>
         <td>Melanie Martinez</td>
         <td><img src="https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/c/0/6/e/c06e5b6d6a73bd671dd40e3f4e5b43fb.jpg" width="65px"></td>
         <td><a href="https://www.facebook.com/melaniemartinezmusic">FB</td>
       </tr>
       <tr>
-        <td>1</td>
+        <td>4</td>
         <td>Lana Del Rey</td>
         <td><img src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Lana_Del_Rey_Releases_Music_Video_For_New_Track_%27Burning_Desire%279.jpg" width="65px"></td>
         <td><a href="https://www.facebook.com/lanadelrey/">FB</td>
       </tr>
       <tr>
-        <td>1</td>
+        <td>5</td>
         <td>AURORA</td>
         <td><img src="https://upload.wikimedia.org/wikipedia/commons/f/fe/Aurora_Stavernfestivalen_2018_%28191706%29_%28cropped%29.jpg" width="65px"></td>
         <td><a href="https://www.facebook.com/iamAURORA/">FB</td>
