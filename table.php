b00eaa27b2e85e7ab2d79ae81943d57189c10900
Piamsuk <97584435+Piamsuk@users.noreply.github.com>
2022-03-05 13:49:17 +0700

    Update table.php

--- table.php
+++ table.php
@@ -13,8 +13,8 @@
       <tr>
         <td>1</td>
         <td></td>
-        <td><img src="..." width="45px"></td>
-        <td><a href="...">Twitter</td>
+        <td><img src="https://th.m.wikipedia.org/wiki/%E0%B9%84%E0%B8%9F%E0%B8%A5%E0%B9%8C:170514_BLACKPINK_at_YG_x_UNICEF_Walking_Festival_07.jpg" width="45px"></td>
+        <td><a href="https://twitter.com/blackpink?s=21">Twitter</td>
       </tr>
     </table>
   </body>
